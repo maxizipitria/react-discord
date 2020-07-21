478553877b61f53d20ffb2a0f5f3b9177d7334de
Maxi Zipitria <maxi.zipitria@gmail.com>
2020-07-21 19:56:14 -0300

    Small changes

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,35 @@ import { ReactDiscord } from "./reconciler";
 import { Embed } from "./components/embed.component";
 import { Text } from "./components/text.component";
 import { RootContainer } from "./RootContainer";
-import { DiscordEvents } from "./discord/discord-events";
+
+import { IEmbedProps } from "./interfaces/IEmbedElementProps";
+import { ITextProps } from "./interfaces/ITextElementProps";
+import { IEmbedField } from "./interfaces/IEmbedField";
+
+import {
+  DiscordEvents,
+  ReactionDetails,
+  ReactionEvent,
+  VoiceStateUpdateEvent,
+} from "./discord/discord-events";
 import { DiscordS } from "./discord";
+import { IReactionConfig } from "./interfaces/IReactionConfig";
+
+export {
+  Embed,
+  Text,
+  RootContainer,
+  // Interfaces
+  IEmbedProps,
+  ITextProps,
+  IEmbedField,
+  IReactionConfig,
+  // Discord
+  DiscordS,
+  DiscordEvents,
+  ReactionDetails,
+  ReactionEvent,
+  VoiceStateUpdateEvent,
+};
 
 export default ReactDiscord;
-
-export { DiscordS, DiscordEvents, Embed, Text, RootContainer };
